1ae2abab1a33e1aaf910b561d91505bfe0b01a3e
nilay <nilayshirke52@gmail.com>
2023-09-12 10:02:49 +0530

    Hello World

--- Assignment 1/HelloWorld.c
+++ Assignment 1/HelloWorld.c
@@ -1,10 +1,14 @@
 #include <omp.h>
 #include <stdio.h>
-
+void mssg()
+{
+    int n;
+    n = omp_get_thread_num();
+    printf("\n Hello World no %d \n", n);
+}
 int main()
 {
-#pragma omp parallel 
-    printf("Hello World\n");
-
-return 0;
+#pragma omp parallel num_threads(10)
+    mssg();
+    return 0;
 }
